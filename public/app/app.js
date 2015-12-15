044dbd091bc985da7e474b4e4d8badd45c0909bb
Polina Soshnin <psoshnin@wellesley.edu>
2015-12-14 16:30:04 -0800

    Add languages factory

--- public/app/app.js
+++ public/app/app.js
@@ -1,29 +1,53 @@
-var gitStats = angular.module("gitStats", ["chart.js"])
+var gitStats = angular.module("gitStats", [
+  'chart.js'])
 
-gitStats.controller("LineController", function ($scope) {
+gitStats.controller("LineController", function ($scope, Languages) {
 
-  $scope.labels = ['Ruby', 'JavaScript', 'Java', 'HTML', 
-  'Python', 'CSS', 'C#', 'Objective-C', 'Shell', 'C', 'Rust', 'PHP'];
+  $scope.labels = [];
   $scope.series = ['Series A'];
+  $scope.data = [];
 
-  $scope.data = [
-    [29, 15, 6, 6, 4, 3, 2,2,2,1,1,1,1]
-  ];
+  $scope.getLanguages = function() {
+    var langs = Languages.getLanguages();
 
-  var languages = 
-  { Ruby: 29,
-  JavaScript: 15,
-  Java: 6,
-  HTML: 6,
-  Python: 4,
-  CSS: 3,
-  'C#': 2,
-  'C++': 2,
-  'Objective-C': 2,
-  Shell: 1,
-  C: 1,
-  Rust: 1,
-  PHP: 1 };
+    console.log("Fetching languages from the factory");
+    console.log("languages", langs);
 
-});
+    var series = [];
+
+    for (var item in langs) {
+      $scope.labels.push(item);
+      series.push(langs[item]);
+    }
+    
+    $scope.data.push(series);
+  }
+
+  $scope.getLanguages();
+})
+
+gitStats.factory('Languages', function ($http) {
+
+  var getLanguages = function() {
+    var languages = 
+    { Ruby: 29,
+    JavaScript: 15,
+    Java: 6,
+    HTML: 6,
+    Python: 4,
+    CSS: 3,
+    'C#': 2,
+    'C++': 2,
+    'Objective-C': 2,
+    Shell: 1,
+    C: 1,
+    Rust: 1,
+    PHP: 1 };
+    return languages;
+  }
+
+  return {
+    getLanguages: getLanguages
+  };
+})
     
---
+++ public/app/services.js
@@ -0,0 +1,28 @@
+// angular.module('services', [])
+
+// .factory('Languages', function ($http) {
+
+//   var getLanguages = function() {
+//     var languages = 
+//     { Ruby: 29,
+//     JavaScript: 15,
+//     Java: 6,
+//     HTML: 6,
+//     Python: 4,
+//     CSS: 3,
+//     'C#': 2,
+//     'C++': 2,
+//     'Objective-C': 2,
+//     Shell: 1,
+//     C: 1,
+//     Rust: 1,
+//     PHP: 1 };
+//     };
+//     return languages;
+//   }
+
+//   return {
+//     getLanguages: getLanguages
+//   };
+// })
+
